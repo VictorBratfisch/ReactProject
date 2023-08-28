c640ae0d1e2ab7cfc6093a9edd2a6cdf35671ea0
VictorBratfisch <victorbratfisch22@gmail.com>
2023-08-28 17:04:30 -0300

    cep finder

---
+++ cep_finder/src/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./App.css";
+import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha algum cep!");
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Erro ao buscar CEP!");
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default App;
